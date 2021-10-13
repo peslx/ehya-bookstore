36775d79b82993d4674ee9d4d181e1880306d603
peslx <90468331+peslx@users.noreply.github.com>
2021-10-14 02:31:47 +0900

    Created: scripts for mobile-menu and modal-window

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,39 @@
 $(document).ready(function () {
-  $(".navbar-button").click(function (e) {
+  $(".navbar-button").click(function () {
     $(".navbar-button__bg").toggleClass("navbar-button__bg--active");
     $(".container--mobile").toggleClass("container--mobile--visible");
     $(".menu--mobile").toggleClass("menu--mobile--visible");
   });
+
+  // console.log($(".popup"));
+
+  $(".castModal").click(function () {
+    openModal();
+    // console.log("Modal => opened");
+  });
+
+  $(".modal__close, .modal__wrapper").click(function () {
+    closeModal();
+  });
+
+  $(document).keydown(function (e) {
+    if (e.key === "Escape") {
+      closeModal();
+      closeMobile();
+    }
+  });
+
+  function closeMobile() {
+    $(".navbar-button__bg").removeClass("navbar-button__bg--active");
+    $(".container--mobile").removeClass("container--mobile--visible");
+    $(".menu--mobile").removeClass("menu--mobile--visible");
+  }
+
+  function openModal() {
+    closeMobile();
+    $(".modal").addClass("modal--active");
+  }
+  function closeModal() {
+    $(".modal").removeClass("modal--active");
+  }
 });
